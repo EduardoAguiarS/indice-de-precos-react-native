8366eef5ba06cfe411872ef8363ca248c881d7ee
Eduardo Aguiar <edu.pessoal@outlook.com>
2024-02-24 18:20:08 -0300

    padding

--- components/FormScreeen.js
+++ components/FormScreeen.js
@@ -105,9 +105,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 10,
-    width: '100%',
-
+    padding: 20,
   },
   title: {
     fontSize: 16,
